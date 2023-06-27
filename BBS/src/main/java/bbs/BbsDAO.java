2b3f8e4469b8e753e1b7718af3432c9adc56a881
Lee Jaeweon <vngjtu5890@gmail.com>
2023-06-27 15:55:14 +0900

    게시판 완성

--- BBS/src/main/java/bbs/BbsDAO.java
+++ BBS/src/main/java/bbs/BbsDAO.java
@@ -71,4 +71,66 @@ public class BbsDAO {
 		}
 		return -1; // 데이터베이스 오류
 	}
+	public ArrayList<Bbs> getList(int pageNumber){
+		String SQL = "SELECT * FROM BBS WHERE bbsID < ? AND bbsAvailable = 1 ORDER BY bbsID DESC LIMIT 10";
+		ArrayList<Bbs> list = new ArrayList<Bbs>();
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, getNext()-(pageNumber -1)*10);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				Bbs bbs = new Bbs();
+				bbs.setBbsID(rs.getInt(1));
+				bbs.setBbsTitle(rs.getString(2));
+				bbs.setUserID(rs.getString(3));
+				bbs.setBbsDate(rs.getString(4));
+				bbs.setBbsContent(rs.getString(5));
+				bbs.setBbsAvailable(rs.getInt(6));
+				list.add(bbs);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return list; 
+	}
+	// 해당 페이지로 넘어갈 수 있는지 검사 
+	public boolean nextPage(int pageNumber){
+		String SQL = "SELECT * FROM BBS WHERE bbsID < ? AND bbsAvailable = 1";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, getNext()-(pageNumber -1)*10);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				return true;
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return false; 
+	}
+	
+	//하나의 게시글을 보는 메소드
+	public Bbs getBbs(int bbsID)
+    {
+		String SQL = "SELECT * FROM BBS WHERE bbsID = ?"; 
+        try {
+				PreparedStatement pstmt = conn.prepareStatement(SQL);
+				pstmt.setInt(1, bbsID);
+				rs = pstmt.executeQuery();
+				if (rs.next())
+				{
+	                Bbs bbs = new Bbs();
+	                bbs.setBbsID(rs.getInt(1));
+	                bbs.setBbsTitle(rs.getString(2));
+	                bbs.setUserID(rs.getString(3));
+	                bbs.setBbsDate(rs.getString(4));
+	                bbs.setBbsContent(rs.getString(5));
+	                bbs.setBbsAvailable(rs.getInt(6));
+	                return bbs;
+		        }
+        } catch (Exception e) {
+        	e.printStackTrace();
+        }
+        return null; 
+    }
 }
